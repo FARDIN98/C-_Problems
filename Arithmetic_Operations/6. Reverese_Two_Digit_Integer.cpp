8ff70e77a6bd58424d258ec4ba8be6ff7e79215e
FARDIN98 <fardinahmed66@gmail.com>
2021-09-14 23:04:59 +0600

    loops problem solving to be continued

--- Arithmetic_Operations/6. Reverese_Two_Digit_Integer.cpp
+++ Arithmetic_Operations/6. Reverese_Two_Digit_Integer.cpp
@@ -1,15 +1,15 @@
-/*6. Write a program that reverses a 2 digit positive number without using any loop.*/
+/*6. Write a program that remerses a 2 digit positive number without using any loop.*/
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int a,rev;
+    int a,rem;
     cout << "Enter an Integer: ";
     cin >> a;
-    rev = a % 10;   //21 % 10
-    rev *= 10;   // 1 * 10
-    a /= 10;  //21 / 10 = 2
-    rev += a;  // 10 + 2 = 12
+    rem = a % 10;   //21 % 10 = 1 [rem =1]
+    rem = rem*10;   // 1 * 10 = 10
+    a = a/10;  //21 / 10 = 2 [a=21]
+    rem = rem + a;  // 10 + 2 = 12
     return 0;
 }
